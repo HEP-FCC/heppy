7e0e224edeaf45e53deee10449c84dcd1ef20547
Valentin Volkl <valentin.volkl@cern.ch>
2020-02-27 11:40:51 +0100

    Update setup

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 
 
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-centos7-gcc62-opt/setup.sh
-
+source /cvmfs/fcc.cern.ch/sw/latest/setup.sh
 
 export HEPPY=$PWD/heppy
 export PATH=$HEPPY/bin:$PATH
